3a9679396e05036d247e0e09dbfb84eab8d6b6d0
Dani Calvente <danicalvente@hotmail.com>
2022-11-17 15:51:19 +0100

    first changes on multi-language feature and some styling to the banner

--- script.js
+++ script.js
@@ -16,4 +16,27 @@ function select() {
   visibleMenu = false;
 }
 
+// Multi-language feature
+const flagsElement = document.getElementById("flags");
+
+flagsElement.addEventListener("click", (e) => {
+  changeLanguage(e.target.parentElement.dataset.language);
+});
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
 // function that swicht the language
+
+const changeLanguage = async (language) => {
+  const requestJson = await fetch(`./languages/${language}.json`);
+  const texts = await requestJson.json();
+
+  for (const textToChange of textsToChange) {
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
+
+    //console.log(section, value);
+
+    textToChange.innerHTML = texts[section][value];
+  }
+};
